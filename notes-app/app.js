eda8f3c61ab9d4f8dbeeb943066027950bae079f
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-22 14:42:47 -0400

    Used chalk to color console output.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,20 @@
-const validator = require('validator');
 const notes = require('./notes.js');
 const app = notes();
 
 console.log(app);
 
-console.log(validator.isURL('ftp://mead.io'));
+/*
+    Challenge: Use the chalk library in your project
+
+    1. Install version of chalk
+    2. Load chalk into app.js
+    3. Use it to print the string "Success!" to the console in green
+    4. Test your work
+
+    Bonus: Use docs to mess around with other styles. Make text bold and inversed
+*/
+
+const chalk = require('chalk');
+
+console.log(chalk.green("Success!"));
+console.log(chalk.bold.inverse("Bye!"));
