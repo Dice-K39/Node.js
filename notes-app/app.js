303affd942831308d0c5aa8c906fe368b3cc880d
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-23 11:46:41 -0400

    Getting input from users through command line.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,20 +1,15 @@
 const notes = require('./notes.js');
-const app = notes();
-
-console.log(app);
-
-/*
-    Challenge: Use the chalk library in your project
-
-    1. Install version of chalk
-    2. Load chalk into app.js
-    3. Use it to print the string "Success!" to the console in green
-    4. Test your work
-
-    Bonus: Use docs to mess around with other styles. Make text bold and inversed
-*/
-
 const chalk = require('chalk');
 
-console.log(chalk.green("Success!"));
-console.log(chalk.bold.inverse("Bye!"));
+const command = process.argv[2];
+
+console.log(process.argv);
+
+if (command === "add")
+{
+    console.log("Adding note!");
+}
+else if (command === "remove")
+{
+    console.log("Removing note!");
+}
