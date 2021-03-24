1b47474ac3562d43c8a8beba09ae0ca7d3d931a5
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-24 15:17:01 -0400

    Added a new option for add command to include the description of the note.

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,21 +5,38 @@ const chalk = require('chalk');
 // Customize yargs version
 yargs.version("1.1.0");
 
-// add, remove, read, list
 /*
-    Challenge: Add two new commands
+    Challenge: Add an option to yargs
 
-    1. Setup command to support "list" command (print placeholder message for now)
-    2. Setup command to support "read" command (print placeholder message for now)
-    3. Test your work by running both commands and ensure correct output
+    1. Setup a body option for the add command
+    2. Configure a description, make it required, and for it to be a string
+    3. Log the body value in the handler function
+    4. Test your work
 */
+
+// add, remove, read, list
 yargs.command(
     {
         command: "add",
         describe: "Add a new note",
-        handler: () =>
+        builder:
         {
-            console.log("Adding a new note");
+            title:
+            {
+                describe: "Note Title",
+                demandOption: true,
+                type: "string"
+            },
+            body:
+            {
+                describe: "Note Description",
+                demandOption: true,
+                type: "string"
+            }
+        },
+        handler: (argv) =>
+        {
+            console.log("Title: "+ argv.title + "\nBody: " + argv.body);
         }
     }
 ).command(
