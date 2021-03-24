8157b70f525f2b3ed328185675d41130470e50aa
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-24 14:53:30 -0400

    Used yargs package to create commands for note app.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,52 @@
 const notes = require('./notes.js');
+const yargs = require("yargs");
 const chalk = require('chalk');
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(process.argv);
+// add, remove, read, list
+/*
+    Challenge: Add two new commands
 
-if (command === "add")
-{
-    console.log("Adding note!");
-}
-else if (command === "remove")
-{
-    console.log("Removing note!");
-}
+    1. Setup command to support "list" command (print placeholder message for now)
+    2. Setup command to support "read" command (print placeholder message for now)
+    3. Test your work by running both commands and ensure correct output
+*/
+yargs.command(
+    {
+        command: "add",
+        describe: "Add a new note",
+        handler: () =>
+        {
+            console.log("Adding a new note");
+        }
+    }
+).command(
+    {
+        command: "remove",
+        describe: "Remove a note",
+        handler: () =>
+        {
+            console.log("Removing a note");
+        }
+    }
+).command(
+    {
+        command: "list",
+        describe: "List all notes",
+        handler: () =>
+        {
+            console.log("Listing all notes");
+        }
+    }
+).command(
+    {
+        command: "read",
+        describe: "Read a note",
+        handler: () =>
+        {
+            console.log("Reading a note");
+        }
+    }
+).argv;
