6960e27c3d3a2e08a25dd83f7929faf8fbc4e239
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-29 15:32:01 -0400

    HTTP request to weatherstack.com to get current weather.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,10 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() =>
+const url = "http://api.weatherstack.com/current?access_key=8e5ce20797f413b84d430f5293a69cb7&query=37.8267,-122.4233";
+
+request({ url: url }, (error, response) => 
 {
-    console.log("2 Second Timer");
-}, 2000);
+    const data = JSON.parse(response.body);
 
-setTimeout(() =>
-{
-    console.log("0 Second Timer");
-}, 0);
-
-console.log("Stopping");
+    console.log(data.current);
+});
