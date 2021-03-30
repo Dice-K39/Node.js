866fc6ab26125c6792d749b04c35474306b963a7
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-30 19:02:04 -0400

    Challenge to display actual temperature and feels like temperature of a location.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,9 +2,16 @@ const request = require("request");
 
 const url = "http://api.weatherstack.com/current?access_key=8e5ce20797f413b84d430f5293a69cb7&query=37.8267,-122.4233";
 
-request({ url: url }, (error, response) => 
+request({ url: url, json: true }, (error, response) => 
 {
-    const data = JSON.parse(response.body);
+    const temperature = response.body.current;
 
-    console.log(data.current);
+    console.log("It is currently " + temperature.temperature + " degrees out. It feels like " + temperature.feelslike + " degrees out.");
 });
+
+/* 
+    Challenge: Print a small forecast to the user
+
+    1. Print: "It is currently 9 degrees out. It feels like 5 degrees out."
+    2. Test your work!
+*/
