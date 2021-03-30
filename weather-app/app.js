4fd897109a8e02bc16b67db5b684d8c2a6736221
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-30 19:41:29 -0400

    Retreived latitude and longitude information for a location.

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,9 +9,24 @@ request({ url: url, json: true }, (error, response) =>
     console.log(temperature.weather_descriptions[0] + ". It is currently " + temperature.temperature + " degrees out. It feels like " + temperature.feelslike + " degrees out.");
 });
 
-/* 
-    Challenge: Print a small forecast to the user
 
-    1. Print: "It is currently 9 degrees out. It feels like 5 degrees out."
-    2. Test your work!
+
+/*
+    Geocoding
+    Address -> Lat/Long -> Weather
+
+    Challenge: Print the lat/long for Los Angeles
+
+    1. Fire off a new request to the URL explored in browser
+    2. Have the request module parse it as JSON
+    3. Print both the latitude and longitude to the terminal
+    4. Test your work!
 */
+const geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW1hdGFzb3JhIiwiYSI6ImNrbXduMnh1dDAwdHkydnFrcm11dnVvcDQifQ.AjWGK6HBlKUfXKopLDd9kg&limit=1"
+
+request({ url: geoCodeURL, json: true }, (error, response) =>
+{
+    const location = response.body.features[0];
+
+    console.log("Los Angeles is located on " + location.center[1] + " latitude, " + location.center[0] + " longitude.");
+})
