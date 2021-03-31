565ed8f6c3e79f320b16b706686f7cc35640c9aa
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-31 12:49:43 -0400

    Error handling.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,49 @@
 const request = require("request");
 
-const url = "http://api.weatherstack.com/current?access_key=8e5ce20797f413b84d430f5293a69cb7&query=37.8267,-122.4233&units=f";
+// const url = "http://api.weatherstack.com/current?access_key=8e5ce20797f413b84d430f5293a69cb7&query=37.8267,-122.4233&units=f";
 
-request({ url: url, json: true }, (error, response) => 
-{
-    const temperature = response.body.current;
+// request({ url: url, json: true }, (error, response) => 
+// {
+//     if (error)
+//     {
+//         console.log("Unable to connect to weather service!");
+//     }
+//     else if (response.body.error)
+//     {
+//         console.log("Unable to find location.");
+//     }
+//     else
+//     {
+//         const temperature = response.body.current;
 
-    console.log(temperature.weather_descriptions[0] + ". It is currently " + temperature.temperature + " degrees out. It feels like " + temperature.feelslike + " degrees out.");
-});
-
-
+//         console.log(temperature.weather_descriptions[0] + ". It is currently " + temperature.temperature + " degrees out. It feels like " + temperature.feelslike + " degrees out.");
+//     }
+// });
 
 /*
-    Geocoding
-    Address -> Lat/Long -> Weather
+    Challenge: Handle errors for geocoding request
 
-    Challenge: Print the lat/long for Los Angeles
-
-    1. Fire off a new request to the URL explored in browser
-    2. Have the request module parse it as JSON
-    3. Print both the latitude and longitude to the terminal
-    4. Test your work!
+    1. Setup an error handler for low-level errors
+    2. Test by disabling network request and running the app
+    3. Setup error handling for no matching results
+    4. Test by altering the search term and running the app
 */
 const geoCodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW1hdGFzb3JhIiwiYSI6ImNrbXduMnh1dDAwdHkydnFrcm11dnVvcDQifQ.AjWGK6HBlKUfXKopLDd9kg&limit=1"
 
 request({ url: geoCodeURL, json: true }, (error, response) =>
 {
-    const location = response.body.features[0];
+    if (error)
+    {
+        console.log("Unable to connect to geocoding service!");
+    }
+    else if (response.body.features.length === 0)
+    {
+        console.log("Unable to find location!");
+    }
+    else
+    {
+        const location = response.body.features[0];
 
-    console.log("Los Angeles is located on " + location.center[1] + " latitude, " + location.center[0] + " longitude.");
+        console.log("Los Angeles is located on " + location.center[1] + " latitude, " + location.center[0] + " longitude.");
+    }
 })
