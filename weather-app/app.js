380af4026e7f30b4f935d1ae87b0bd57bbf0f313
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-02 23:53:36 -0400

    Callback chaining and entering a city name on console to get current temperature.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,38 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Atlanta", (error, data) =>
+/* 
+    Challenge: Accept location via command line argument
+
+    1. Access the command line argument without yargs
+    2. Use the string value as the input for geocode
+    3. Only geocode if a location was provided
+    4. Test your work with a couple of locations
+*/
+
+const query = process.argv[2];
+
+if (!query)
 {
-    console.log("Error", error);
-    console.log("Data", data);
-});
-
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+    console.log("Please provide a city.");
+}
+else
+{
+    geocode(query, (error, data) =>
+    {
+        if (error)
+        {
+            return console.log("Error", error);   
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => 
+        {
+            if (error)
+            {
+                return console.log('Error', error);
+            }
+    
+            console.log("Location: " + data.location);
+            console.log(forecastData);
+        });
+    });
+}
