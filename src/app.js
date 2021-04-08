cef195855430d0e036fccdc8d639da63c9b6d5a5
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-08 16:39:00 -0400

    Challenge to send HTML and a JSON object to a webpage.

--- src/app.js
+++ src/app.js
@@ -4,30 +4,44 @@ const app = express();
 
 app.get("", (req, res) =>
 {
-    res.send("Hello express!");
+    res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) =>
 {
-    res.send("Help page");
+    res.send(
+        [
+            {
+                name: "Dice"
+            },
+            {
+                name: "Jack"
+            }
+        ]
+    );
 });
 
 /* 
-    Challenge: Setup two new routes
+    Challenge: Update routes
 
-    1. Setup an about route and render a page title
-    2. Setup a weather route and renger a page title
+    1. Setup about route to render a title with HTML
+    2. Setup a weather route to send back JSON Object with forecast and location strings
     3. Test your work by visiting both in the browser
 */
 
 app.get("/about", (req, res) =>
 {
-    res.send("About Page");
+    res.send("<h1>About Page</h1>");
 });
 
 app.get("/weather", (req, res) =>
 {
-    res.send("Weather Forecast");
+    res.send(
+        {
+            forecast: "Sunny with a chance of rain",
+            location: "Atlanta"
+        }
+    );
 });
 
 app.listen(3000, () =>
