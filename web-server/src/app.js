f063fae18ab0154e5faaa1d82458c2d756cdce11
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-09 20:03:12 -0400

    Used hbs, express.js view engine for handlebars.js.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,43 @@ const path = require("path");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) =>
+{
+    res.render("index",
+    {
+        title: "Weather",
+        name: "Dice"
+    });
+});
+
+app.get("/about", (req, res) =>
+{
+    res.render("about",
+    {
+        title: "About me",
+        name: "Dice"
+    });
+});
+
+/*
+    Challenge: Create a template for help page
+
+    1. Setup a help template to render a help message to the screen
+    2. Setup the help route and render the template with an example message
+    3. Visit the route in the browser and see your help message print
+*/
+
+app.get("/help", (req, res) =>
+{
+    res.render("help",
+    {
+        title: "Help page",
+        message: "Sorry. No help for you!"
+    });
+});
 
 app.get("/weather", (req, res) =>
 {
