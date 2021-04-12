6b817d7134aa237bae100b361c4799b026f2887e
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-12 12:41:16 -0400

    Used fetch to fetch weather for a city.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log("Client side javascript file is loaded!");
+
+fetch("http://puzzle.mead.io/puzzle")
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+/*
+    Challenge: Fetch weather!
+
+    1. Setup a call to fetch to fetch weather for Boston
+    2. Get the parse JSON response
+        - If error property, print error
+        - If no error property, print location and forecast
+    3. Refresh the browser and test your work
+*/
+fetch("http://localhost:3000/weather?address=boston")
+    .then(res => res.json())
+    .then(data => data.error ? console.log(data.error) : console.log(data.location, data.temperature));
