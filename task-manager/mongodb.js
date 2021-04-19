f2d10756ba00d07196af5ca9d38d1298b7782a0e
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-19 09:55:00 -0400

    Inserted documents into database.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,10 +14,77 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: 
 
     const db = client.db(databaseName)
 
-    db.collection("users").insertOne(
-        {
-            name: "Dice",
-            age: 40
-        }
-    )
+    // db.collection("users").insertOne(
+    //     {
+    //         name: "Dice",
+    //         age: 40
+    //     }
+    // , (error, result) =>
+    // {
+    //     if (error)
+    //     {
+    //         return console.log("Unable to insert user")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection("users").insertMany(
+    //     [
+    //         {
+    //             name: "John",
+    //             age: 100
+    //         },
+    //         {
+    //             name: "Jen",
+    //             age: 26
+    //         }
+    //     ]
+    // , (error, result) =>
+    // {
+    //     if (error)
+    //     {
+    //         console.log("Unable to insert documents!")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    /*
+        Challenge: Insert 3 tasks into a new tasks collection
+
+        1. Use insertMany to insert the documents
+            - description (string), completed(boolean)
+        2. Setup the callback to handle error or print ops
+        3. Run the script
+        4. Refresh the database in Robo 3t and view data in tasks collection
+    */
+   db.collection("tasks").insertMany(
+       [
+           {
+               description: "Wake up",
+               completed: true
+           },
+           {
+               description: "Get COVID-19 vaccine",
+               completed: false
+           },
+           {
+               description: "Get lunch",
+               completed: false
+           },
+           {
+               description: "Go to work",
+               completed: false
+           }
+       ], (error, result) =>
+       {
+           if (error)
+           {
+               console.log("Unable to insert documents!")
+           }
+
+           console.log(result.ops)
+       }
+   )
 })
