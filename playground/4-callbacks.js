0b05e35392372aae53bef67c16c72fa68d1e2f4b
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-20 23:06:56 -0400

    Exploring Promises.

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -28,23 +28,34 @@
 //     console.log(data);
 // });
 
-/*
-    Challenge: Mess around with the callback pattern
+// const add = (num1, num2, callback) =>
+// {
+//     setTimeout(() =>
+//     {
+//         callback(num1 + num2);
+//     }, 2000);
 
-    1. Define an add function that accepts the correct arguments
-    2. Use setTimeout to simulate a 2 second delay
-    3. After 2 seconds are up, call the callback function with the sum
-    4. Test your work!
-*/
-const add = (num1, num2, callback) =>
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
+
+const doWorkCallback = (callback) =>
 {
     setTimeout(() =>
     {
-        callback(num1 + num2);
-    }, 2000);
-
+        // callback("This is my error!", undefined)
+        callback(undefined, [1, 4, 7])
+    }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallback((error, result) =>
+{
+    if (error)
+    {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
