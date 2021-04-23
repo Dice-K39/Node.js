6055853d1ccfa81fc40298d7fa121e0aa6a93547
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-23 15:35:29 -0400

    Added validation to password in User and added validation to Task.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -8,6 +8,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
     useUnifiedTopology: true
 });
 
+/*
+    Challenge 1: Add a password field to User
+
+    1. Setup the field as a required string
+    2. Ensure the length is greater than 6
+    3. Trim the password
+    4. Ensure that password doesn't contain "password"
+    5. Test your work!
+*/
+
 const User = mongoose.model("User",
 {
     name:
@@ -30,6 +40,20 @@ const User = mongoose.model("User",
             }
         }
     },
+    password:
+    {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value)
+        {
+            if (value.toLowerCase().includes("password"))
+            {
+                throw new Error("Password is invalid");
+            }
+        }
+    },
     age:
     {
         type: Number,
@@ -44,44 +68,54 @@ const User = mongoose.model("User",
     }
 });
 
-const me = new User(
-   {
-       name: "  Dice   ",
-       email: "Dice@email.com"
-   }
-);
-
-me.save().then(() =>
-{
-    console.log(me);
-}).catch(error =>
-{
-    console.log("Error!", error);
-});
-
-// const Task = mongoose.model("Task",
-// {
-//     description:
-//     {
-//         type: String
-//     },
-//     completed:
-//     {
-//         type: Boolean
-//     }
-// });
-
-// const task = new Task(
-//     {
-//         description: "Buy jeans",
-//         completed: false
-//     }
+// const me = new User(
+//    {
+//        name: "  Dice   ",
+//        password: "helloworld123",
+//        email: "Dice@email.com"
+//    }
 // );
 
-// task.save().then(() =>
+// me.save().then(() =>
 // {
-//     console.log(task);
-// }).catch((error) =>
+//     console.log(me);
+// }).catch(error =>
 // {
 //     console.log("Error!", error);
 // });
+
+/*
+    Challenge 2: Add validation and sanitization to task
+
+    1. Trim the description and make it required
+    2. Make completed optional and default it to false
+    3. Test your work with and without errors
+*/
+const Task = mongoose.model("Task",
+{
+    description:
+    {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed:
+    {
+        type: Boolean,
+        default: false
+    }
+});
+
+const task = new Task(
+    {
+        description: "    Study JavaScript    "
+    }
+);
+
+task.save().then(() =>
+{
+    console.log(task);
+}).catch((error) =>
+{
+    console.log("Error!", error);
+});
