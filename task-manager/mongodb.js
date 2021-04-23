fbde49783701a0572ed479138c21842e6468f7ec
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-23 14:05:32 -0400

    Installed and used Mongoose to insert data into MongoDB database.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -29,14 +29,6 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: 
     //     console.log(error);
     // })
 
-    /*
-        Challenge: Use deleteOne to remove a task
-
-        1. Grab the description for the task you want to remove
-        2. Setup the call with the query
-        3. Use promise methods to setup the success/error handlers
-        4. Test your work!
-    */
     db.collection("tasks").deleteOne(
         {
             description: "Go to work"
---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
+{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const User = mongoose.model("User",
+{
+    name:
+    {
+        type: String
+    },
+    age:
+    {
+        type: Number
+    }
+});
+
+const me = new User(
+    {
+        name: "Dice",
+        age: "Drew"
+    }
+);
+
+me.save().then(() =>
+{
+    console.log(me);
+}).catch(error =>
+{
+    console.log("Error!", error);
+});
