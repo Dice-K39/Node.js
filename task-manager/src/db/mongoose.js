b2a8bc3197a801e683f8b111c7847787c98d6d28
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-23 14:45:39 -0400

    Installed validator to validate and sanitize data before inserting into database.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
 {
@@ -11,60 +12,76 @@ const User = mongoose.model("User",
 {
     name:
     {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:
+    {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value)
+        {
+            if (!validator.isEmail(value))
+            {
+                throw new Error("Email is invalid");
+            }
+        }
     },
     age:
     {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value)
+        {
+            if (value < 0)
+            {
+                throw new Error("Age must be a positive number");
+            }
+        }
     }
 });
 
-// const me = new User(
-//     {
-//         name: "Dice",
-//         age: "Drew"
-//     }
-// );
-
-// me.save().then(() =>
-// {
-//     console.log(me);
-// }).catch(error =>
-// {
-//     console.log("Error!", error);
-// });
-
-/* 
-    Challenge: Create a model for tasks
-
-    1. Define the model with description and completed fields
-    2. Create a new instance of the model
-    3. Save the model to the database
-    4. Test your work
-*/
-const Task = mongoose.model("Task",
-{
-    description:
-    {
-        type: String
-    },
-    completed:
-    {
-        type: Boolean
-    }
-});
-
-const task = new Task(
-    {
-        description: "Buy jeans",
-        completed: false
-    }
+const me = new User(
+   {
+       name: "  Dice   ",
+       email: "Dice@email.com"
+   }
 );
 
-task.save().then(() =>
+me.save().then(() =>
 {
-    console.log(task);
-}).catch((error) =>
+    console.log(me);
+}).catch(error =>
 {
     console.log("Error!", error);
 });
+
+// const Task = mongoose.model("Task",
+// {
+//     description:
+//     {
+//         type: String
+//     },
+//     completed:
+//     {
+//         type: Boolean
+//     }
+// });
+
+// const task = new Task(
+//     {
+//         description: "Buy jeans",
+//         completed: false
+//     }
+// );
+
+// task.save().then(() =>
+// {
+//     console.log(task);
+// }).catch((error) =>
+// {
+//     console.log("Error!", error);
+// });
