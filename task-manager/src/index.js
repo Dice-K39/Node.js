208bde6aba0917aa89ea0f8bfdd8f87709750917
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-24 23:28:11 -0400

    Challenge to separate code concerns to separate files and create documents into database using postman.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 require("./db/mongoose");
 const express = require("express");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,7 +14,27 @@ app.post("/users", (req, res) =>
 
     user.save().then(() =>
     {
-        res.send(user);
+        res.status(201).send(user);
+    }).catch((error) =>
+    {
+        res.status(400).send(error);
+    });
+});
+
+/*
+    Goal: Setup the task creation endpoint
+
+    1. Create a separate file for the task model (load it into index.js)
+    2. Create the task creation endpoint (handle success and error)
+    3. Test the endpoint from postman with good and bad data
+*/
+app.post("/tasks", (req, res) =>
+{
+    const task = new Task(req.body);
+
+    task.save().then(() =>
+    {
+        res.status(201).send(task);
     }).catch((error) =>
     {
         res.status(400).send(error);
