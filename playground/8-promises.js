b627dbe7d5f7fbc147a5303a25287f5a4fb3abb8
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-25 10:01:54 -0400

    Learned about promise chaining and its syntax.

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,16 +1,56 @@
-const doWorkPromise = new Promise((resolve, reject) => 
-{
-    setTimeout(() =>
-    {
-        // resolve([1, 2, 3])
-        reject("Things went wrong!")
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => 
+// {
+//     setTimeout(() =>
+//     {
+//         // resolve([1, 2, 3])
+//         reject("Things went wrong!")
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) =>
+// doWorkPromise.then((result) =>
+// {
+//     console.log("Success!", result)
+// }).catch((error) =>
+// {
+//     console.log("Error!", error)
+// })
+
+const add = (a, b) =>
 {
-    console.log("Success!", result)
+    return new Promise((resolve, reject) =>
+    {
+        setTimeout(() =>
+        {
+            resolve(a + b);
+        }, 2000);
+    });
+}
+
+// add(1, 2).then((sum) =>
+// {
+//     console.log(sum);
+
+//     add(sum, 5).then((sum2) =>
+//     {
+//         console.log(sum2);
+//     }).catch((error) =>
+//     {
+//         console.log(error);
+//     });
+// }).catch((error) =>
+// {
+//     console.log(error);
+// });
+
+add(1, 1).then((sum) =>
+{
+    console.log(sum);
+
+    return add(sum, 4);
+}).then((sum2) =>
+{
+    console.log(sum2);
 }).catch((error) =>
 {
-    console.log("Error!", error)
-})
+    console.log(error);
+});
