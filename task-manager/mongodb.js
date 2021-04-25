2ddd55e53e889e9df040fa0bef00454a7bc1d1da
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-24 23:05:42 -0400

    Recap of REST API and adding a document to the database.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,41 +3,41 @@
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectID
 
-const { MongoClient, ObjectID } = require("mongodb")
+// const { MongoClient, ObjectID } = require("mongodb")
 
-const connectionURL = "mongodb://127.0.0.1:27017" // local host IP
-const databaseName = "task-manager"
+// const connectionURL = "mongodb://127.0.0.1:27017" // local host IP
+// const databaseName = "task-manager"
 
-MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => // useUnifiedTopology to get rid of deprecation warning
-{
-    if (error)
-    {
-        return console.log("Unable to connect to database.")
-    }
+// MongoClient.connect(connectionURL, { useUnifiedTopology: true, useNewUrlParser: true }, (error, client) => // useUnifiedTopology to get rid of deprecation warning
+// {
+//     if (error)
+//     {
+//         return console.log("Unable to connect to database.")
+//     }
 
-    const db = client.db(databaseName)
+//     const db = client.db(databaseName)
 
-    // db.collection("users").deleteMany(
-    //     {
-    //         age: 20
-    //     }
-    // ).then(result =>
-    // {
-    //     console.log(result);  
-    // }).catch(error =>
-    // {
-    //     console.log(error);
-    // })
+//     // db.collection("users").deleteMany(
+//     //     {
+//     //         age: 20
+//     //     }
+//     // ).then(result =>
+//     // {
+//     //     console.log(result);  
+//     // }).catch(error =>
+//     // {
+//     //     console.log(error);
+//     // })
 
-    db.collection("tasks").deleteOne(
-        {
-            description: "Go to work"
-        }
-    ).then(result =>
-    {
-        console.log(result);
-    }).catch(error =>
-    {
-        console.log(error);
-    });
-})
+//     db.collection("tasks").deleteOne(
+//         {
+//             description: "Go to work"
+//         }
+//     ).then(result =>
+//     {
+//         console.log(result);
+//     }).catch(error =>
+//     {
+//         console.log(error);
+//     });
+// })
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
 {
@@ -8,89 +7,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
     useUnifiedTopology: true
 });
 
-/*
-    Challenge 1: Add a password field to User
-
-    1. Setup the field as a required string
-    2. Ensure the length is greater than 6
-    3. Trim the password
-    4. Ensure that password doesn't contain "password"
-    5. Test your work!
-*/
-
-const User = mongoose.model("User",
-{
-    name:
-    {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email:
-    {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value)
-        {
-            if (!validator.isEmail(value))
-            {
-                throw new Error("Email is invalid");
-            }
-        }
-    },
-    password:
-    {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value)
-        {
-            if (value.toLowerCase().includes("password"))
-            {
-                throw new Error("Password is invalid");
-            }
-        }
-    },
-    age:
-    {
-        type: Number,
-        default: 0,
-        validate(value)
-        {
-            if (value < 0)
-            {
-                throw new Error("Age must be a positive number");
-            }
-        }
-    }
-});
-
-// const me = new User(
-//    {
-//        name: "  Dice   ",
-//        password: "helloworld123",
-//        email: "Dice@email.com"
-//    }
-// );
-
-// me.save().then(() =>
-// {
-//     console.log(me);
-// }).catch(error =>
-// {
-//     console.log("Error!", error);
-// });
-
-/*
-    Challenge 2: Add validation and sanitization to task
-
-    1. Trim the description and make it required
-    2. Make completed optional and default it to false
-    3. Test your work with and without errors
-*/
 const Task = mongoose.model("Task",
 {
     description:
@@ -104,18 +20,4 @@ const Task = mongoose.model("Task",
         type: Boolean,
         default: false
     }
-});
-
-const task = new Task(
-    {
-        description: "    Study JavaScript    "
-    }
-);
-
-task.save().then(() =>
-{
-    console.log(task);
-}).catch((error) =>
-{
-    console.log("Error!", error);
 });
