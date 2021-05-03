42da5beaed56b8f24b2c8a00b525081f2526988e
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-02 23:20:45 -0400

    Refactored Task routes so that only the user that owns the task can modify it.

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -24,11 +24,19 @@ router.post("/tasks", auth, async (req, res) =>
     }
 });
 
-router.get("/tasks", async (req, res) =>
+/*
+    Goal: Refactor GET /tasks
+
+    1. Add authentication
+    2. Return tasks only for the authenticated user
+    3. Test your work!
+*/
+
+router.get("/tasks", auth, async (req, res) =>
 {
     try
     {
-        const tasks = await Task.find({});
+        const tasks = await Task.find({ owner: req.user._id });
 
         res.send(tasks);
     } 
@@ -38,13 +46,13 @@ router.get("/tasks", async (req, res) =>
     }
 });
 
-router.get("/tasks/:id", async (req, res) =>
+router.get("/tasks/:id", auth, async (req, res) =>
 {
     const _id = req.params.id;
 
     try
     {
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({ _id, owner: req.user._id });
 
         if (!task)
         {
@@ -59,7 +67,7 @@ router.get("/tasks/:id", async (req, res) =>
     }
 });
 
-router.patch("/tasks/:id", async (req, res) => 
+router.patch("/tasks/:id", auth, async (req, res) => 
 {
     const updates = Object.keys(req.body);
     const allowedUpdates = ["description", "completed"];
@@ -72,17 +80,17 @@ router.patch("/tasks/:id", async (req, res) =>
 
     try
     {
-        const task = await Task.findById(req.params.id);
-
-        updates.forEach((update) => task[update] = req.body[update]);
-
-        await task.save();
-
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
+        
         if (!task)
         {
             return res.status(404).send();
         }
 
+        updates.forEach((update) => task[update] = req.body[update]);
+
+        await task.save();
+        
         res.send(task);
     }
     catch (error)
@@ -91,11 +99,19 @@ router.patch("/tasks/:id", async (req, res) =>
     }
 });
 
-router.delete("/tasks/:id", async (req, res) =>
+/*
+    Goal: Refactor DELETE /tasks/:id
+
+    1. Add authentication
+    2. Find the task by _id/owner (findOneAndDelete)
+    3. Test your work!
+*/
+
+router.delete("/tasks/:id", auth, async (req, res) =>
 {
     try
     {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
         if (!task)
         {
