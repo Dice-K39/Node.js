43581f7e6e0e6520a9a5a8ac32b6b57e27929009
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-09 02:53:59 -0400

    Added environment variable for personal email account.

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -8,7 +8,7 @@ const sendWelcomeEmail = (email, name) =>
     sgMail.send(
         {
             to: email,
-            from: "daisukehashimoto39@gmail.com",
+            from: process.env.GMAIL_ACCOUNT,
             subject: "Thanks for joining in!",
             text: `Welcome to the app, ${name}. Let me know how you get along with the app`
         }
@@ -20,7 +20,7 @@ const sendCancelationEmail = (email, name) =>
     sgMail.send(
         {
             to: email,
-            from: "daisukehashimoto39@gmail.com",
+            from: process.env.GMAIL_ACCOUNT,
             subject: "Sorry to see you go. . .",
             text: `Can you give me a reason that you are leaving ${name}?`
         }
@@ -36,7 +36,7 @@ const sendCancelationEmail = (email, name) =>
 //             service: "gmail",
 //             auth: 
 //             {
-//                 user: "daisukehashimoto39@gmail.com",
+//                 user: process.env.GMAIL_ACCOUNT,
 //                 pass: process.env.GMAIL_PASSWORD
 //             }
 //         }
@@ -50,22 +50,13 @@ const sendCancelationEmail = (email, name) =>
 //     transporterObj.sendMail(
 //         {
 //             to: email,
-//             from: "daisukehashimoto39@gmail.com",
+//             from: process.env.GMAIL_ACCOUNT,
 //             subject: "Thanks for joining in!",
 //             text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
 //         }
 //     );
 // }
 
-// /*
-//     Goal: Send email to user on cancelation
-
-//     1. Setup a new function for sending an email on cancelation
-//         - email and name as args
-//     2. Include their name in the email and ask why they canceled
-//     3. Call it just after the account is removed
-//     4. Run the request and check your inbox!
-// */
 // const sendCancelationEmail = (email, name) =>
 // {
 //     const transporterObj = transporter();
@@ -73,7 +64,7 @@ const sendCancelationEmail = (email, name) =>
 //     transporterObj.sendMail(
 //         {
 //             to: email,
-//             from: "daisukehashimoto39@gmail.com",
+//             from: process.env.GMAIL_ACCOUNT,
 //             subject: "Reason for Cancelation",
 //             text: `We are sorry to see you go. Please let us know the reason for your departure ${name}`
 //         }
