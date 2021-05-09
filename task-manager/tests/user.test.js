51c3bcba195e932a5bec312163588f58da589b13
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-09 11:15:32 -0400

    Installed supertest to test express application. Learned that jest has lifecycle methods to do something before and after each test case.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,25 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = 
+{
+    name: "Mike",
+    email: "mike@example.com",
+    password: "56what!!"
+}
+
+beforeEach(async () =>
+{
+    await User.deleteMany();
+    await new User(userOne).save();
+});
+
+afterAll(() =>
+{
+    mongoose.connection.close();
+})
 
 test("Should signup a new user", async () =>
 {
@@ -9,5 +29,33 @@ test("Should signup a new user", async () =>
             email: "Dice@example.com",
             password: "MyPass777!"
         }
-    ).expect(201)
+    ).expect(201);
+});
+
+test("Should login existing user", async () =>
+{
+    await request(app).post("/users/login").send(
+        {
+            email: userOne.email,
+            password: userOne.password
+        }
+    ).expect(200);
+});
+
+/*
+    Goal: Test login fail
+
+    1. Create "Should not login nonexistent user"
+    2. Send off the request with bad credentials
+    3. Expect the correct status response
+    4. Test your work!
+*/
+test("Should not login nonexistent user", async () =>
+{
+    await request(app).post("/users/login").send(
+        {
+            email: "helloworld@example.com",
+            password: "1234567890"
+        }
+    ).expect(400);
 });
