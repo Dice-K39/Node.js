a718e1f73307809ba52b2b715c635f44f94cb59d
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-10 12:31:05 -0400

    Authenticated user to test routes.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,13 +1,22 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = 
 {
+    _id: userOneId,
     name: "Mike",
     email: "mike@example.com",
-    password: "56what!!"
+    password: "56what!!",
+    tokens:
+    [
+        {
+            token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+        }
+    ]
 }
 
 beforeEach(async () =>
@@ -42,14 +51,6 @@ test("Should login existing user", async () =>
     ).expect(200);
 });
 
-/*
-    Goal: Test login fail
-
-    1. Create "Should not login nonexistent user"
-    2. Send off the request with bad credentials
-    3. Expect the correct status response
-    4. Test your work!
-*/
 test("Should not login nonexistent user", async () =>
 {
     await request(app).post("/users/login").send(
@@ -58,4 +59,47 @@ test("Should not login nonexistent user", async () =>
             password: "1234567890"
         }
     ).expect(400);
+});
+
+test("Should get profile for user", async () =>
+{
+    await request(app)
+        .get("/users/me")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () =>
+{
+    await request(app)
+        .get("/users/me")
+        .send()
+        .expect(401);
+});
+
+/*
+    Goal: Test delete account
+
+    1. Create "Should delete account for user"
+        - Setup auth header and expect correct status code
+    2. Create "Should not delete account for unauthenticated user"
+        - Expect correct status code
+    3. Test your work!
+*/
+test("Should delete account for user", async () =>
+{
+    await request(app)
+        .delete("/users/me")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+});
+
+test("Should not delete account for unauthenticated user", async () =>
+{
+    await request(app)
+        .delete("/users/me")
+        .send()
+        .expect(401);
 });
