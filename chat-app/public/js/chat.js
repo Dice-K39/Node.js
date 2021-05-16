58928e10708f81df3e73308f75d415ae0da27e4c
Dice-K39 <daisukehashimoto39@gmail.com>
2021-05-16 12:32:42 -0400

    User interface enhancements.

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,18 +1,30 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) =>
 {
     console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (event) =>
+$messageForm.addEventListener("submit", (event) =>
 {
     event.preventDefault();
+
+    $messageFormButton.setAttribute("disabled", "diabled");
 
     const message = event.target.elements.message.value;
 
     socket.emit("sendMessage", message, (error) =>
     {
+        $messageFormButton.removeAttribute("disabled");
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+
         if (error)
         {
             return console.log(error);
@@ -23,20 +35,22 @@ document.querySelector("#message-form").addEventListener("submit", (event) =>
 });
 
 /*
-    Goal: Setup acknowledgment
+    Goal: Disable the send location button while location being sent
 
-    1. Setup the client acknowledgment function
-    2. Setup the server to send back the acknowledgment
-    3. Have the client print "Location shared!" when acknowledged
+    1. Set up a selector at the top of the file
+    2. Disable the button just before getting the current position
+    3. Enable the button in the acknowledgment callback
     4. Test your work!
 */
-document.querySelector("#send-location").addEventListener("click", () =>
+$sendLocationButton.addEventListener("click", () =>
 {
     if (!navigator.geolocation)
     {
         return alert("Geolocation is not supported by your browser.");
     }
 
+    $sendLocationButton.setAttribute("disabled", "disabled");
+    
     navigator.geolocation.getCurrentPosition((position) =>
     {
         socket.emit("sendLocation", 
@@ -45,6 +59,8 @@ document.querySelector("#send-location").addEventListener("click", () =>
             longitude: position.coords.longitude
         }, (error) =>
         {
+            $sendLocationButton.removeAttribute("disabled");
+
             if (error)
             {
                 return console.log(error);
